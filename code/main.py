c946b9a7d821171f0befcce340a7f099062023c4
Edward Huh <edward_huh@brown.edu>
2022-05-01 12:17:55 -0400

    completed kmeans with 3 clusters

--- code/main.py
+++ code/main.py
@@ -3,9 +3,9 @@ from concurrent.futures import process
 from pathlib import Path
 
 import matplotlib.pyplot as plt
-import tensorflow as tf
-
 import preprocess
+import tensorflow as tf
+from kmeans import get_kmeans
 from visualize_boxes import visualize
 
 
@@ -31,31 +31,37 @@ def main():
 
         processed_data_dir = Path(data_dir) / "processed"
         resized_img_dir = processed_data_dir / "resized"
-        resized_one_file = processed_data_dir / "resized_one_file"
+        resized_one_file_dir = processed_data_dir / "resized_one_file"
         ####
         assert raw_img_dir.exists()
 
-        # Step 1: resize all images to 256 * 256, store to `resize_img_dir`
-        x = preprocess.parse_data(
-            raw_img_dir,
-            annotation_train_dir,
-            resized_img_dir,
-            save_data=True,
-        )
-        # Step 2: read in processed json data
-        y = preprocess.get_data(resized_img_dir / "annotations.json")
+        # # Step 1: resize all images to 256 * 256, store to `resize_img_dir`
+        # x = preprocess.parse_data(
+        #     raw_img_dir,
+        #     annotation_train_dir,
+        #     resized_img_dir,
+        #     save_data=True,
+        # )
+        # # Step 2: read in processed json data
+        # y = preprocess.get_data(resized_img_dir / "annotations.json")
 
-        assert x == y
+        # assert x == y
 
-        preprocess.parse_data_single(
-            raw_img_dir, annotation_train_dir, resized_one_file
-        )
+        # preprocess.parse_data_single(
+        #     raw_img_dir, annotation_train_dir, resized_one_file_dir
+        # )
 
-        # Visualize a single image
-        batch = preprocess.get_batch(y)
-        # img = tf.keras.preprocessing.image.array_to_img(z)
-        # plt.imshow(img)
-        visualize(filepath=y[0]["file"], bounding_boxes=y[0]["boxes"], found_boxes=[])
+        # # Visualize a single image
+        # batch = preprocess.get_batch(y)
+        # # img = tf.keras.preprocessing.image.array_to_img(z)
+        # # plt.imshow(img)
+        # visualize(filepath=y[0]["file"], bounding_boxes=y[0]["boxes"], found_boxes=[])
+
+    # Get KMeans
+    centers = get_kmeans(
+        resized_one_file_dir=resized_one_file_dir,
+        annotation_filename=annotation_train_dir,
+    )
 
     return None
 
