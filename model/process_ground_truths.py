4c6300b7b0ec13bcc9cca1adb0859b43c60140e0
Paul Xu <digicosmos@gmail.com>
2022-05-01 21:55:42 -0400

    figured out tf.Dataset training pipeline

--- model/process_ground_truths.py
+++ model/process_ground_truths.py
@@ -2,16 +2,25 @@ import numpy as np
 import tensorflow as tf
 
 GRID_SIZES = [13, 26]
+anchor_boxes = [
+    [216, 216],
+    [16, 30],
+    [33, 23],
+    [30, 61],
+    [62, 45],
+    [59, 119],
+]
 
 
-def correct_ground_truths(ground_truths, anchor_boxes, grid_sizes=GRID_SIZES):
+def correct_ground_truths(
+    ground_truths, anchor_boxes=anchor_boxes, grid_sizes=GRID_SIZES
+):
     """
     Processes the ground truths into the same shape as the outputs of YOLO head
 
-    ground_truths: a (m, b, 4) tensor with all the correct bounding box information
+    ground_truths: a (b, 4) tensor with all the correct bounding box information
         shaped as (bx, by, bw, bh)
 
-        m: batch size
         b: the number of true bounding boxes
         bx, by: the bounding box center coordinates of values between (0, 1) relative to
             the resized image (typically 416)
@@ -21,11 +30,10 @@ def correct_ground_truths(ground_truths, anchor_boxes, grid_sizes=GRID_SIZES):
     grid_sizes: a list of the grid sizes at different scales of the model outputs
 
     returns:
-        a list of tensors of shapes (m, grid_y, grid_x, n_anFchors, 6), each corresponding to a different scale
+        a list of tensors of shapes (grid_y, grid_x, n_anchors, 6), each corresponding to a different scale
     """
 
-    batch_size = ground_truths.shape[0]
-    n_bounding_boxes = ground_truths.shape[1]
+    n_bounding_boxes = ground_truths.shape[0]
     n_anchors = len(anchor_boxes) // 2
     anchor_boxes = np.array(anchor_boxes).reshape(-1, n_anchors, 2)
 
@@ -33,21 +41,19 @@ def correct_ground_truths(ground_truths, anchor_boxes, grid_sizes=GRID_SIZES):
 
     for grid_size, anchors in zip(grid_sizes, anchor_boxes):
 
-        output_tensor = np.zeros(
-            [batch_size, grid_size, grid_size, n_anchors, 6], dtype=np.float32
-        )
+        output_tensor = np.zeros([grid_size, grid_size, n_anchors, 6], dtype=np.float32)
         cell_size = 1.0 / grid_size
 
-        anchors = np.tile(anchors / 416, (batch_size, n_bounding_boxes, 1, 1))
+        anchors = np.tile(anchors / 416, (n_bounding_boxes, 1, 1))
         intersect_mins = np.maximum(
-            np.expand_dims(ground_truths[:, :, 2:4], axis=2), anchors
+            np.expand_dims(ground_truths[:, 2:4], axis=1), anchors
         )
         intersect_maxes = np.minimum(
-            np.expand_dims(ground_truths[:, :, 2:4], axis=2), anchors
+            np.expand_dims(ground_truths[:, 2:4], axis=1), anchors
         )
         intersect_wh = np.maximum(intersect_maxes - intersect_mins, 0.0)
         intersect_area = intersect_wh[..., 0] * intersect_wh[..., 1]
-        box_area = ground_truths[:, :, 2] * ground_truths[:, :, 3]
+        box_area = (ground_truths[:, 2] * ground_truths[:, 3]).reshape((-1, 1))
         anchor_area = anchors[..., 0] * anchors[..., 1]
         iou = intersect_area / (box_area + anchor_area - intersect_area + 1e-10)
 
@@ -55,16 +61,14 @@ def correct_ground_truths(ground_truths, anchor_boxes, grid_sizes=GRID_SIZES):
 
         # the downside of doing this is if there are multiple ground boxes in the same cell, the ground truth will be overwritten
 
-        for i in range(batch_size):
-            for j in range(n_bounding_boxes):
-                grid_x = int(ground_truths[i, j, 0] // cell_size)
-                grid_y = int(ground_truths[i, j, 1] // cell_size)
+        for j in range(n_bounding_boxes):
+            grid_x = int(ground_truths[j, 0] // cell_size)
+            grid_y = int(ground_truths[j, 1] // cell_size)
 
-                output_tensor[i, grid_y, grid_x, best_anchor[j], :4] = ground_truths[
-                    i,
-                    j,
-                ]
-                output_tensor[i, grid_y, grid_x, best_anchor[j], 4:] = 1.0
+            output_tensor[grid_y, grid_x, best_anchor[j], :4] = ground_truths[
+                j,
+            ]
+            output_tensor[grid_y, grid_x, best_anchor[j], 4:] = 1.0
 
         outputs.append(output_tensor)
 
@@ -72,7 +76,7 @@ def correct_ground_truths(ground_truths, anchor_boxes, grid_sizes=GRID_SIZES):
 
 
 if __name__ == "__main__":
-    ground_truths = np.array([[[0, 0, 0.5, 0.5]]])
+    ground_truths = np.array([[0, 0, 0.5, 0.5]])
     anchor_boxes = [
         [216, 216],
         [16, 30],
@@ -83,4 +87,4 @@ if __name__ == "__main__":
     ]
 
     outputs = correct_ground_truths(ground_truths, anchor_boxes)
-    assert outputs[0, 0, 0, 0, 4] == 1.0
+    assert outputs[0][0, 0, 0, 4] == 1.0
--- model/train.py
+++ model/train.py
@@ -1,17 +1,14 @@
 import tensorflow as tf
 import numpy as np
 
+from process_ground_truths import correct_ground_truths, anchor_boxes
+from yolov3_tiny import YOLOv3_Tiny
+
 
 def process_image(img_path):
     img = tf.io.read_file(img_path)
     img = tf.cast(tf.image.decode_jpeg(img, channels=3), tf.float32) / 255.0
-    return img, img_path
-
-
-def process_csv(csv_path):
-    labels = tf.io.read_file(csv_path)
-    labels = tf.strings.split(labels, '\r\n')
-    return labels
+    return img
 
 
 image_dataset = tf.data.Dataset.list_files(
@@ -20,14 +17,41 @@ image_dataset = tf.data.Dataset.list_files(
 
 label_dataset = tf.data.Dataset.list_files(
     "../data/processed/resized_one_file/*_416.txt", shuffle=False
-).map(process_csv)
+)
 
+dataset = tf.data.Dataset.zip((image_dataset, label_dataset)).batch(20)
 
-# dataset = tf.data.Dataset.zip((image_dataset, label_dataset))
+# for record, label in image_dataset.take(2):
+#     print(
+#         record,
+#         np.loadtxt(
+#             label.numpy().decode("utf-8").replace(".jpg", ".txt"),
+#             dtype=np.float32,
+#             delimiter=",",
+#         ),
+#     )
 
-for record, label in image_dataset.take(2):
-    print(record, np.loadtxt(
-        label.numpy().decode("utf-8").replace(".jpg", ".txt"),
-        dtype=np.float32,
-        delimiter=","
-        ))
+model = YOLOv3_Tiny(
+    anchors=anchor_boxes, n_classes=1, iou_threshold=0.5, score_threshold=0.5
+)
+
+for image, label in dataset:
+    # y1, y2 = model(image)
+    y1s_true = []
+    y2s_true = []
+
+    for label in label.numpy():
+        label = label.decode("utf-8")
+        ground_truths = np.loadtxt(label, dtype=np.float32, delimiter=",").reshape(
+            (-1, 4)
+        )
+        y1, y2 = correct_ground_truths(ground_truths)
+        y1s_true.append(y1)
+        y2s_true.append(y2)
+
+    y1_true = tf.convert_to_tensor(y1s_true)
+    y2_true = tf.convert_to_tensor(y2s_true)
+
+    print(y1_true.shape)
+    print(y2_true.shape)
+    break
